fc884149e9c07a8d96a13e0412229f36bdf57ab5
alishakhadka <alissakhadka@gmail.com>
2024-03-06 12:59:35 +0545

    contact page is added

--- testproject/urls.py
+++ testproject/urls.py
@@ -20,7 +20,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',views.home),
-    # path('contact/', views.contact),
+    path('contact/', views.contact),
     # path('about/', views.about)
 
 
